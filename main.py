7cc00be414c95c039bb2d5e0f1b15036967ce92b
mason-9506 <mason.9506@kakaocorp.com>
2026-01-15 22:47:50 +0900

    ✨ Implement main

---
+++ main.py
@@ -0,0 +1,28 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse
+from containers import Container
+from user.interface.controllers.user_controller import router as user_router
+
+app = FastAPI()
+app.container = Container()
+
+app.include_router(user_router)
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=400,
+        content=exc.errors(),
+    )
+
+
+@app.get("/")
+def hello():
+    return {"Hello": "FastAPI"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
