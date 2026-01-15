e45562a820dc39d33ced42d3c158f78e0f63422d
mason-9506 <mason.9506@kakaocorp.com>
2026-01-15 22:46:34 +0900

    ✨ Implement user controller (interface)

---
+++ user/interface/controllers/user_controller.py
@@ -0,0 +1,74 @@
+from datetime import datetime
+
+from dependency_injector.wiring import Provide, inject
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel, EmailStr, Field
+
+from containers import Container
+from user.application.user_service import UserService
+
+router = APIRouter(prefix="/users")
+
+
+class CreateUserBody(BaseModel):
+    name: str = Field(min_length=2, max_length=32)
+    password: str = Field(max_length=64)
+    email: EmailStr = Field(min_length=8, max_length=32)
+
+
+class UserResponse(BaseModel):
+    id: str
+    name: str
+    email: str
+    created_at: datetime
+    updated_at: datetime
+
+
+class UpdateUser(BaseModel):
+    name: str | None = Field(min_length=2, max_length=32, default=None)
+    password: str | None = Field(min_length=8, max_length=32, default=None)
+
+
+@router.post("", status_code=201, response_model=UserResponse)
+@inject
+def create_user(
+    user: CreateUserBody,
+    user_service: UserService = Depends(Provide[Container.user_service]),
+):
+    created_user = user_service.create_user(
+        name=user.name, email=user.email, password=user.password
+    )
+    return created_user
+
+
+@router.put("/{user_id}")
+@inject
+def update_user(
+    user_id: str,
+    user: UpdateUser,
+    user_service: UserService = Depends(Provide[Container.user_service]),
+):
+    updated_user = user_service.update_user(
+        user_id=user_id, name=user.name, password=user.password
+    )
+    return updated_user
+
+
+@router.get("")
+@inject
+def get_users(
+    page: int = 1,
+    items_per_page: int = 10,
+    user_service: UserService = Depends(Provide[Container.user_service]),
+):
+    total_count, users = user_service.get_users(page, items_per_page)
+    return {"total_count": total_count, "page": page, "users": users}
+
+
+@router.delete("", status_code=204)
+@inject
+def delete_user(
+    user_id: str,
+    user_service: UserService = Depends(Provide[Container.user_service]),
+):
+    user_service.delete_user(user_id)
