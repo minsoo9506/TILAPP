378e815423d442a586fd2332ac93e477244d45bb
mason-9506 <mason.9506@kakaocorp.com>
2026-01-15 22:47:07 +0900

    ✨ Implement dependency injection container

---
+++ containers.py
@@ -0,0 +1,14 @@
+from dependency_injector import containers, providers
+from user.infra.repository.user_repo import UserRepository
+from user.application.user_service import UserService
+
+# dependency injection 은 IoC 컨테이너를 사용하여 객체의 생성과 의존성 관리를 담당
+class Container(containers.DeclarativeContainer):
+    wiring_config = containers.WiringConfiguration(
+        modules=["user.interface.controllers.user_controller"]
+    )
+    user_repo = providers.Factory(UserRepository)
+    user_service = providers.Factory(
+        UserService,
+        user_repo=user_repo,
+    )
