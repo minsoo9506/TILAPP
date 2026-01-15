e3b5f6650ab9bd5ad9c379add30df31838f86a64
mason-9506 <mason.9506@kakaocorp.com>
2026-01-15 22:44:54 +0900

    ✨ Implement user repo interface (user domain)

---
+++ user/domain/repository/user_repo.py
@@ -0,0 +1,29 @@
+from abc import ABCMeta, abstractmethod
+
+from user.domain.user import User
+
+
+class IUserRepository(metaclass=ABCMeta):
+    @abstractmethod
+    def save(self, user: User):
+        raise NotImplementedError
+
+    @abstractmethod
+    def find_by_email(self, email: str) -> User:
+        raise NotImplementedError
+
+    @abstractmethod
+    def find_by_id(self, id: str) -> User:
+        raise NotImplementedError
+
+    @abstractmethod
+    def update(self, user: User):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_users(self, page: int, items_per_page: int) -> tuple[int, list[User]]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def delete_user(self, id: str) -> None:
+        raise NotImplementedError
