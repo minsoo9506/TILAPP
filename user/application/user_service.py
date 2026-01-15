5fc65cb94039232bf34b6c12123a0e2b4fc861a7
mason-9506 <mason.9506@kakaocorp.com>
2026-01-15 22:45:23 +0900

    ✨ Implement user service (app)

---
+++ user/application/user_service.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+
+from dependency_injector.wiring import inject
+from fastapi import HTTPException
+from ulid import ULID
+
+from user.domain.repository.user_repo import IUserRepository
+from user.domain.user import User
+from utils.crypto import Crypto
+
+
+class UserService:
+    @inject
+    def __init__(self, user_repo: IUserRepository):
+        self.user_repo = user_repo
+        self.crypto = Crypto()
+        self.ulid = ULID()
+
+    def create_user(
+        self, name: str, email: str, password: str, memo: str | None = None
+    ) -> User:
+        _user = None
+
+        try:
+            _user = self.user_repo.find_by_email(email)
+        except HTTPException as e:
+            if e.status_code != 422:
+                raise e
+
+        if _user:
+            raise HTTPException(
+                status_code=422, detail="User with this email already exists"
+            )
+
+        now = datetime.now()
+        user: User = User(
+            id=self.ulid.generate(),
+            name=name,
+            email=email,
+            password=self.crypto.encrypt(password),
+            created_at=now,
+            updated_at=now,
+            memo=memo,
+        )
+        self.user_repo.save(user)
+        return user
+
+    def update_user(
+        self, user_id: str, name: str | None = None, password: str | None = None
+    ):
+        user = self.user_repo.find_by_id(user_id)
+        if name:
+            user.name = name
+        if password:
+            user.password = self.crypto.encrypt(password)
+        user.updated_at = datetime.now()
+
+        self.user_repo.update(user)
+        return user
+
+    def get_users(self, page: int, items_per_page: int) -> tuple[int, list[User]]:
+        return self.user_repo.get_users(page, items_per_page)
+
+    def delete_user(self, user_id: str) -> None:
+        self.user_repo.delete_user(user_id)
