0b7efbd8c06b36be8451a673059793d1e54b4205
minsoo9506 <ghktjd15gh@gmail.com>
2026-01-17 00:39:09 +0900

    ✨ Implement jwt access process

---
+++ common/auth.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+from fastapi.security import OAuth2PasswordBearer
+from typing import Annotated
+from datetime import datetime, timedelta
+from fastapi import HTTPException, status, Depends
+from jose import JWTError, jwt
+from enum import StrEnum
+
+SECRET_KEY = "your_secret_key"
+ALGORITHM = "HS256"
+
+class Role(StrEnum):
+    ADMIN = "admin"
+    USER = "user"
+
+def create_access_token(payload: dict, role: Role, expires_delta: timedelta = timedelta(hours=6)):
+    expire = datetime.now() + expires_delta
+    payload.update({"exp": expire, "role": role})
+    encoded_jwt = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")
+
+@dataclass
+class CurrentUser:
+    id: str
+    role: Role
+
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]) -> CurrentUser:
+    payload = decode_access_token(token)
+    user_id: str = payload.get("user_id")
+    role: str = payload.get("role")
+    if user_id is None or role is None or role != Role.USER:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+    return CurrentUser(id=user_id, role=Role(role))
+
+def get_admin_user(token: Annotated[str, Depends(oauth2_scheme)]) -> CurrentUser:
+    payload = decode_access_token(token)
+    role: Role = payload.get("role")
+    if  role is None or role != Role.ADMIN:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+    return CurrentUser(id="ADMIN_USER_ID", role=role)
