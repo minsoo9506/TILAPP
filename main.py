479ee60887a2369cba369d3a477fa4b3c5d75b9d
minsoo9506 <ghktjd15gh@gmail.com>
2026-01-18 22:26:55 +0900

    ✨ Add middleware and logger

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.requests import Request
 from fastapi.responses import JSONResponse
 from containers import Container
+from middlewares import create_middlewares
 from user.interface.controllers.user_controller import router as user_router
 from note.interface.controllers.note_controller import router as note_router
 
@@ -12,6 +13,8 @@ app.container = Container()
 
 app.include_router(user_router)
 app.include_router(note_router)
+
+create_middlewares(app)
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
